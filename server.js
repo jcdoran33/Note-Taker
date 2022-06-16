402018a8319e550e2f61c3efa808b0b76123854c
Jack Doran <jcdoran33@gmail.com>
2022-06-16 19:28:40 -0400

    created first api GET routing in server.js. Added routes folder

--- server.js
+++ server.js
@@ -2,24 +2,25 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+//add api const definition (should link to index.js? in routes folder?)
+const api = require("routes/index.js"); //unsure if I need this
 //require UUID for user ID - use uudiv4(); to call
 const { v4: uuidv4 } = require('uuid');
 //variable definitions - app and PORT
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-//parse incoming stuff
+//parse incoming stuff - middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/api", api); //unsure if I need this
 //designate the public folder
 app.use(express.static('public'));
 
 //main content
 
 //HTML routes:
-//GET for main (/)
-
 //GET /notes - return notes.html
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
@@ -28,9 +29,13 @@ app.get("/notes", (req, res) => {
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 });
+
 //API routes:
 //GET api/notes should read the db.json file, and return all saved noes as JSON
-
+app.get("/api/notes", (req, res) => {
+    res.status(200).json(`${req.method} request received to get notes`);
+    console.info(`${req.method} requst received to get notes`);
+});
 //POST to apit/notes should receive a new note to save on the request body, add it to the db.json file, then return new note to client.
 //Note - use a UUID generator
 
