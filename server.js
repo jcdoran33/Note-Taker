6814eacee0542038437bca73d4c35fc8dad7a68d
Jack Doran <jcdoran33@gmail.com>
2022-06-16 18:54:39 -0400

    fixed file structure, removed files from public folder

--- server.js
+++ server.js
@@ -21,11 +21,13 @@ app.use(express.static('public'));
 //GET for main (/)
 
 //GET /notes - return notes.html
-app.get("/notes", (req,res) => {
+app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 //GET * should return the index.html
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"))
+});
 //API routes:
 //GET api/notes should read the db.json file, and return all saved noes as JSON
 
